c4bb48cfeddfbda61b824d2bdd2ab1196db7e60a
root <root@vgr.centos7.dev>
2017-08-18 10:32:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WebPackDemo
+WebPack学习
+
---
+++ hello/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  devtool: 'eval-source-map', //source-map	
+  entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+  output: {
+    path: __dirname + "/public",//打包后的文件存放的地方
+    filename: "bundle.js"//打包后输出文件的文件名 [hash]
+  },
+  devServer: {
+    contentBase: "./public",//本地服务器所加载的页面所在的目录
+    historyApiFallback: true,//不跳转
+    compress: true,
+    port: 8077,
+    inline: true//实时刷新
+  }
+}
